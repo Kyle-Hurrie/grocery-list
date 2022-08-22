7aef0beb33d5018d2ab0aae406ae754c69af030e
khurr <khurrie72@gmail.com>
2022-08-22 16:26:05 +0200

    button name change

--- src/components/groceryList/GroceryList.tsx
+++ src/components/groceryList/GroceryList.tsx
@@ -95,7 +95,7 @@ const GroceryList = () => {
             type={'button'}
             onClick={() => handleAdd()}
           >
-            Submit
+            Add Item
           </button>
         </div>
       </div>
